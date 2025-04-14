a06fdc812934728608eff0c5458d61016dc2b32f
Maciej Potręć <potrec123@proton.me>
2025-04-14 19:30:12 +0200

    feat: add functional leds

---
+++ project/src/hardware/leds.cpp
@@ -0,0 +1,56 @@
+#include "leds.h"
+
+#define LED_RED_PIN 13
+#define LED_GREEN_PIN 12
+#define LED_BLUE_PIN 14
+#define LED_YELLOW_PIN 27
+
+void initLeds()
+{
+    pinMode(LED_RED_PIN, OUTPUT);
+    pinMode(LED_GREEN_PIN, OUTPUT);
+    pinMode(LED_BLUE_PIN, OUTPUT);
+    pinMode(LED_YELLOW_PIN, OUTPUT);
+}
+
+void turnLedOn(int ledIndex)
+{
+    switch (ledIndex)
+    {
+    case 0:
+        digitalWrite(LED_RED_PIN, HIGH);
+        break;
+    case 1:
+        digitalWrite(LED_GREEN_PIN, HIGH);
+        break;
+    case 2:
+        digitalWrite(LED_BLUE_PIN, HIGH);
+        break;
+    case 3:
+        digitalWrite(LED_YELLOW_PIN, HIGH);
+        break;
+    default:
+        break;
+    }
+}
+
+void turnLedOff(int ledIndex)
+{
+    switch (ledIndex)
+    {
+    case 0:
+        digitalWrite(LED_RED_PIN, LOW);
+        break;
+    case 1:
+        digitalWrite(LED_GREEN_PIN, LOW);
+        break;
+    case 2:
+        digitalWrite(LED_BLUE_PIN, LOW);
+        break;
+    case 3:
+        digitalWrite(LED_YELLOW_PIN, LOW);
+        break;
+    default:
+        break;
+    }
+}
--- project/src/main.cpp
+++ project/src/main.cpp
@@ -1,38 +1,19 @@
 #include <Arduino.h>
-
-#define LED_RED_PIN 13
-#define PUSH_BUTTON 12
-#define BUZZER 14
-#define LED_YELLOW_PIN 27
-
-int tones[] = {262, 294, 330, 349, 392, 440, 494, 523};
-int currentTone = 0;
-bool wasPressed = false;
+#include <hardware/leds.h>
 
 void setup()
 {
   Serial.begin(921600);
 
-  pinMode(LED_RED_PIN, OUTPUT);
-  pinMode(PUSH_BUTTON, INPUT_PULLUP);
-  pinMode(BUZZER, OUTPUT);
+  initLeds();
 }
 
 void loop()
 {
-  int buttonValue = digitalRead(PUSH_BUTTON);
-  if (buttonValue == LOW && !wasPressed)
+  for (int i = 0; i < 4; i++)
   {
-    tone(BUZZER, tones[currentTone]);
-    digitalWrite(LED_RED_PIN, HIGH);
-    wasPressed = true;
-  }
-  else if (buttonValue == HIGH && wasPressed)
-  {
-    wasPressed = false;
-    digitalWrite(LED_RED_PIN, LOW);
-    noTone(BUZZER);
-    wasPressed = false;
-    currentTone = (currentTone + 1) % 8;
+    turnLedOn(i);
+    delay(500);
+    turnLedOff(i);
   }
 }
